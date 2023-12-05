07af0229e14c33c69d3ed6d057b60b4342bae91e
Quentin Gillet <42375010+Quentin-Gillet@users.noreply.github.com>
2023-12-05 18:58:31 +0100

    Fix auto save for sql

--- .config/nvim/lua/plugins/auto-save.lua
+++ .config/nvim/lua/plugins/auto-save.lua
@@ -1,6 +1,7 @@
 return {
     {
         "Pocco81/auto-save.nvim",
+        -- enabled=false,
         opts = {
             execution_message = {
                 message = function()
@@ -9,6 +10,16 @@ return {
                 dim = 0.18,
                 cleaning_interval = 1250,
             },
+            condition = function(buf)
+                local fn = vim.fn
+                local utils = require("auto-save.utils.data")
+
+                -- don't save for `sql` file types
+                if utils.not_in(fn.getbufvar(buf, "&filetype"), {'sql'}) then
+                    return true
+                end
+                return false
+            end
         },
     },
 }
